5509a78a7c78e4b704c3c130bbda204482210bfd
Doom <clement.doumergue@epitech.eu>
2018-11-06 13:43:40 -0500

    Add wrappers for x86_64 instructions in/out/sti/cli

--- kernel/include/arch/x86_64/instructions.hpp
+++ kernel/include/arch/x86_64/instructions.hpp
@@ -23,6 +23,91 @@ namespace foros::x86_64::instructions
         : "memory"
         );
     }
+
+    inline void outb(uint8_t value, uint16_t port) noexcept
+    {
+        asm volatile(
+        "outb %0, %1"
+        : /* no output operands */
+        :
+        "a"(value), /* send value through the al register */
+        "dN"(port)  /* send port through the dx register */
+        );
+    }
+
+    inline uint8_t inb(uint16_t port) noexcept
+    {
+        uint8_t value;
+
+        asm volatile(
+        "inb %1, %0"
+        : "=a"(value) /* receive value through the al register */
+        : "dN"(port)  /* send port through the dx register */
+        );
+        return value;
+    }
+
+    inline void outw(uint16_t value, uint16_t port) noexcept
+    {
+        asm volatile(
+        "outw %0, %1"
+        : /* no output operands */
+        :
+        "a"(value), /* send value through the ax register */
+        "dN"(port)  /* send port through the dx register */
+        );
+    }
+
+    inline uint16_t inw(uint16_t port) noexcept
+    {
+        uint16_t value;
+
+        asm volatile(
+        "inw %1, %0"
+        : "=a"(value) /* receive value through the ax register */
+        : "dN"(port)  /* send port through the dx register */
+        );
+        return value;
+    }
+
+    inline void outl(uint32_t value, uint16_t port) noexcept
+    {
+        asm volatile(
+        "outl %0, %1"
+        : /* no output operands */
+        :
+        "a"(value), /* send value through the eax register */
+        "dN"(port)  /* send port through the dx register */
+        );
+    }
+
+    inline uint32_t inl(uint16_t port) noexcept
+    {
+        uint32_t value;
+
+        asm volatile(
+        "inl %1, %0"
+        : "=a"(value) /* receive value through the eax register */
+        : "dN"(port)  /* send port through the dx register */
+        );
+        return value;
+    }
+
+    /** Set interrupt flag */
+    inline void sti() noexcept
+    {
+        asm volatile(
+        "sti"
+        );
+    }
+
+    /** Clear interrupt flag */
+    inline void cli() noexcept
+    {
+        asm volatile(
+        "cli"
+        );
+    }
 }
 
 #endif /* !FOROS_X86_64_INSTRUCTIONS_HPP */
