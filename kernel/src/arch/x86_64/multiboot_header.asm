88ccf424b106a0f4c1e32f2cf361929287a7d6a0
Doom <clement.doumergue@epitech.eu>
2018-12-12 01:39:33 -0500

    Add a few comments where necessary

--- kernel/src/arch/x86_64/multiboot_header.asm
+++ kernel/src/arch/x86_64/multiboot_header.asm
@@ -1,4 +1,7 @@
 section .multiboot_header
+
+; Multiboot 2 compliant header
+; See https://wiki.osdev.org/Multiboot#Header_Format
 header_start:
 	dd 0xE85250D6			; magic number for MultiBoot 2
 	dd 0				; architecture (0 => i386)
@@ -7,6 +10,10 @@ header_start:
 	; checksum
 	dd 0x100000000 - (0xE85250D6 + 0 + (header_end - header_start))
 
+	; The header can contain a list of tags
+	; Those tags can be used to provide or request information to/from the bootloader
+
+	; Empty tag, marking the end of the tag list
 	dw 0				; type
 	dw 0				; flags
 	dd 8				; size
